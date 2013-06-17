3e51e40f9705141b48462a6a7feccd1b60ea5519
tusti dwi <tustie.dwi@gmail.com>
2013-06-17 14:32:14 +0700

    ckeditor
    menambah assets ckeditor

---
+++ coms/assets/ckeditor/config.js
@@ -0,0 +1,25 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	 config.language = 'en';
+	// config.uiColor = '#AADC6E';
+	
+   config.filebrowserBrowseUrl = '../../../../../kcfinder/browse.php?type=files';
+   config.filebrowserImageBrowseUrl = '../../../../../kcfinder/browse.php?type=images';
+   config.filebrowserFlashBrowseUrl = '../../../../../kcfinder/browse.php?type=flash';
+   config.filebrowserUploadUrl = '../../../../../kcfinder/upload.php?type=files';
+   config.filebrowserImageUploadUrl = '../../../../../kcfinder/upload.php?type=images';
+   config.filebrowserFlashUploadUrl = '../../../../../kcfinder/upload.php?type=flash';
+   config.toolbar = [
+	['Preview', 'Print','Save'], ['Bold','Italic','Strike','-','Subscript','Superscript'],['NumberedList','BulletedList','-','Blockquote'],['JustifyLeft','JustifyCenter','JustifyBlock','JustifyRight'],
+	['Link','Unlink','-','PageBreak'],['Find','Replace','-','Maximize'],['Source'],
+	'/',
+	['Format', 'Styles','Font','FontSize','Bold','Italic', 'Underline','-','TextColor','HorizontalRule'],['PasteText','PasteFromWord','-','RemoveFormat'],['Table', 'Image','SpecialChar'],['Outdent','Indent'],
+	['Undo','Redo']
+	] ;
+   
+};
