9af14267e5c729a296b31026daa864ad07fd9d9c
Aryo <aryoxp@gmail.com>
2013-06-20 00:11:22 +0700

    Updating hello module settingmenu and sidemenu example.

--- coms/modules/hello/view/hello.settingmenu.php
+++ coms/modules/hello/view/hello.settingmenu.php
@@ -1,8 +1,8 @@
 <li class="dropdown-submenu">
-  <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-list"></i> Hello
+  <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-th"></i> Hello
   </a>
 	<ul class="dropdown-menu pull-right">
-      <li><a href="<?php echo $this->location($module_url); ?>"><i class="icon-list"></i> Settings</a></li>
-      <li><a href="<?php echo $this->location($module_url); ?>"><i class="icon-list"></i> Install</a></li>
+      <li><a href="<?php echo $this->location($module_url); ?>"><i class="icon-wrench"></i> Settings</a></li>
+      <li><a href="<?php echo $this->location($module_url); ?>"><i class="icon-asterisk"></i> Install</a></li>
   </ul>
 </li>
