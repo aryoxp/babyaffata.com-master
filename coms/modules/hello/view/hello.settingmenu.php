83a0eea069a26393670a842540372582287698cb
Aryo <aryoxp@gmail.com>
2013-06-19 23:44:11 +0700

    Updated COMS, adding module setting menu capability. Example on Hello Module.
    Also fix Hello module sidemenu error.

---
+++ coms/modules/hello/view/hello.settingmenu.php
@@ -0,0 +1,8 @@
+<li class="dropdown-submenu">
+  <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-list"></i> Hello
+  </a>
+	<ul class="dropdown-menu pull-right">
+      <li><a href="<?php echo $this->location($module_url); ?>"><i class="icon-list"></i> Settings</a></li>
+      <li><a href="<?php echo $this->location($module_url); ?>"><i class="icon-list"></i> Install</a></li>
+  </ul>
+</li>
---
+++ coms/modules/hello/view/hello.sidemenu.php
@@ -0,0 +1,9 @@
+<li class="dropdown-submenu">
+  <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-list"></i> Hello
+  </a>
+	<ul class="dropdown-menu pull-right">
+      <li><a href="<?php echo $this->location($module_url); ?>"><i class="icon-list"></i> Side Menu</a></li>
+      <li><a href="<?php echo $this->location($module_url."home"); ?>">
+      <i class="icon-list"></i> Index</a></li>
+  </ul>
+</li>
