bc427ff6fd6dbbb2ea1acccb7d799e2c0612af9f
Aryo <aryoxp@gmail.com>
2013-07-13 20:57:54 +0700

    Remove unused script assets on main website

---
+++ assets/script/home.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: aryo
+ * Date: 7/12/13
+ * Time: 7:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(function() {
+    // Handler for .ready() called.
+    $('#carousel').elastislide();
+});
+
---
+++ library/extcontroller.php
@@ -0,0 +1,69 @@
+<?php
+class extcontroller extends controller {
+
+    private $scripts = array();
+    private $styles = array();
+
+    private $page_title = NULL;
+    private $isAuthenticated = false;
+
+    public $session = NULL;
+    public $authenticatedUser = NULL;
+
+    public function __construct(){
+
+        parent::__construct();
+
+        $this->page_title = $this->config->page_title;
+
+    }
+
+    public function head() {
+
+        $navbar['user'] = $this->authenticatedUser;   //$this->session->get("user");
+        $navbar['modules'] = $this->comsmodules;
+
+        $navbar['userid'] = $this->authenticatedUser->username;
+        $navbar['role']   = $this->authenticatedUser->role;
+
+        $this->view('header.php');
+        $this->view('navbar.php', $navbar);
+        $this->view('sidebar.php', array('modules'=>$this->comsmodules));
+    }
+
+    public function foot() {
+        $this->view('footer.php');
+    }
+
+    public function footblank() {
+        $this->view('footer-nonjs.php');
+    }
+
+    public function add_style($stylepath) {
+        if(file_exists( $this->config->assets_folder . "/" . $stylepath ))
+            $this->styles[]	= $stylepath;
+    }
+
+    public function add_script($scriptpath) {
+        if(file_exists( $this->config->assets_folder . "/" . $scriptpath ))
+            $this->scripts[] = $scriptpath;
+    }
+
+    public function get_scripts() {
+        return $this->scripts;
+    }
+
+    public function get_styles() {
+        return $this->styles;
+    }
+
+    public function page_title($appended_string = NULL) {
+        return $this->page_title . $appended_string;
+    }
+
+    public function no_privileges() {
+        $this->view('error/noprivileges.php');
+        exit;
+    }
+
+}
