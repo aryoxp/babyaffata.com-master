d5a35db6844b0d8d33c46e362a5223aeb1430133
Aryo <aryoxp@gmail.com>
2013-07-13 21:00:02 +0700

    Updating controller to extends new extcontroller

--- controller/home.php
+++ controller/home.php
@@ -1,11 +1,16 @@
 <?php
 
-class controller_home extends controller {
+class controller_home extends extcontroller {
 	public function __construct() {
 		parent::__construct();
 	}	
 	
 	public function index() {
+        $this->add_style('style/home.css');
+        $this->add_style('style/elastislide.css');
+        $this->add_script('script/jquerypp.custom.js');
+        $this->add_script('script/jquery.elastislide.js');
+        $this->add_script('script/home.js');
 		$this->view('header.php');
 		$this->view('home.php');
 		$this->view('footer.php');
